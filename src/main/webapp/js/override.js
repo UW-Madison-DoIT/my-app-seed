7fafb51db8263695e0dba3d1d0b85eff8d6edd32
zeke-witter <david.witter@wisc.edu>
2017-05-09 09:12:27 -0500

    Revert the revert!
    This reverts commit ebe42f0d68e23f071de05267362fec45ab97489d.

---
+++ src/main/webapp/js/override.js
@@ -0,0 +1,32 @@
+define(['angular'], function(angular) {
+  // see https://uw-madison-doit.github.io/uw-frame/configuration.html for howto
+  return angular.module('override', [])
+    .constant('OVERRIDE', {
+      'APP_FLAGS': {
+        'defaultTheme': 'uw-madison',
+        'showSearch': false,
+        'isWeb': false,
+        'loginOnLoad': false,
+      },
+      'NAMES': {
+        'title': 'Widget Creator',
+        'fname': 'widget-creator',
+      },
+      'SERVICE_LOC': {
+        'kvURL': null,
+        'groupURL': 'staticFeeds/groups.json',
+        'sessionInfo': 'staticFeeds/guest-session.json',
+        'shibbolethSessionURL': 'staticFeeds/Shibboleth.sso/Session.json',
+        'templates': 'json/starter-templates',
+        'widgetApi': {
+          // For local testing, change to 'staticFeeds/'
+          'entry': 'data:application/json;base64,',
+          'entrySuffix': '',
+          'entries': null,
+        },
+      },
+      'MISC_URLS': {
+        'rootURL': 'https://my.wisc.edu',
+      },
+    });
+});
