69acb8bc12d5f43f1803e9648499791eb70fbb87
zeke-witter <david.witter@wisc.edu>
2017-09-22 14:48:08 -0500

    Update to widget-creator standard, then rename

--- src/main/webapp/my-app/view-home/directives.js
+++ src/main/webapp/my-app/view-home/directives.js
@@ -2,23 +2,5 @@
 
 define(['angular', 'require'], function(angular, require) {
   return angular.module('my-app.view-home.directives', [])
-  .directive('previewWidget', function() {
-    return {
-      restrict: 'E',
-      transclude: true,
-      scope: {
-        fname: '@',
-      },
-      templateUrl: require.toUrl(
-        '../../portal/widgets/partials/widget-card.html'),
-      controller: 'WidgetCardController',
-      link: function(scope, element, attrs) {
-        scope.$watch('fname', function(newValue, oldValue) {
-          if (newValue) {
-            scope.initializeWidget(newValue);
-          }
-        });
-      },
-    };
-  });
+
 });
