b9a7e8046ead172e54bd6f8ee5474aca5dcd9104
zeke-witter <david.witter@wisc.edu>
2017-09-25 11:42:36 -0500

    Add content, unit test, and example architecture

--- src/main/webapp/my-app/view-home/directives.js
+++ src/main/webapp/my-app/view-home/directives.js
@@ -2,5 +2,12 @@
 
 define(['angular', 'require'], function(angular, require) {
   return angular.module('my-app.view-home.directives', [])
-
+    .directive('subview', function() {
+      return {
+        restrict: 'E',
+        templateUrl: require.toUrl('./partials/example-subview.html'),
+        controller: 'HomeViewController',
+        controllerAs: 'vm',
+      };
+    });
 });
